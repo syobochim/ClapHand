cba0d952b01251503fa493fe5c3a40059bfe2c34
syobochim <syobochim@gmail.com>
2020-10-08 02:01:54 +0900

    create visitor page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({ emoji: "👏" })
+  const eventId = new URL(window.location).searchParams.get("id")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://kogh98ozya.execute-api.ap-northeast-1.amazonaws.com/production/claps?id=${encodeURIComponent(eventId)}`)
+      if (!response.ok) {
+        console.error(response);
+      } else {
+        const resJson = await response.json()
+        console.log(resJson)
+        setData({ emoji: resJson.emoji })
+      }
+    };
+    fetchData();
+  }, [])
+
+  const clickHandler = async () => {
+    const response = await fetch(`https://kogh98ozya.execute-api.ap-northeast-1.amazonaws.com/production/claps?id=${encodeURIComponent(eventId)}`, {
+      method: 'PUT'
+    })
+    if (!response.ok) {
+      console.error(response);
+    } else {
+      const resJson = await response.json()
+      console.log('clap! : ' + resJson)
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {eventId}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p id="emoji" onClick={clickHandler}>
+          {data.emoji}
+        </p>
       </header>
     </div>
   );
